8e8694b3df07af99ca5231b92219a8e45d61d0ab
harrman17 <harmankapoor178@gmail.com>
2025-07-02 20:01:24 +0100

    touched up on responsiveness + added list a car button and route to page

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,34 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import Header from './components/Header'
 import Search from './components/Search'
 import Landing from './components/Landing'
+import ListCar from './components/ListCar'
+import { Routes, Route } from 'react-router-dom'
 
 
 function App() {
+  const searchRef = useRef(null)
 
   return (
-    <div className=''>
-      <Header />
-      <Landing />
-      <Search />
+    // main page / landing
+
+    <div>
+        <Routes>
+          <Route path="/" element={
+          <>
+            <Header />
+            <Landing onFindClick={() => searchRef.current.scrollIntoView({ behavior: "smooth" })}/>
+            <div ref={searchRef}>
+              <Search />
+            </div>
+          </>}/>
+          <Route path="list-a-car" element={
+            <>
+              <ListCar />
+            </>}/>
+        </Routes>
     </div>
+
   )
 }
 
--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,17 +1,17 @@
 import React from "react"
 
 
-function Landing() {
+function Landing({ onFindClick }) {
   return (
-    <div className="bg-gray-50 min-h-screen">
-        <div className="p-7 flex flex-col gap-4">
-            <h1 className="font-bold text-3xl">Tell us <span className="text-accent">what matters most</span> - and we'll do the rest</h1>
-            <h2 className="font-medium text-xl">We match you with the best car options based on what you care about</h2>
-            <p>🧠 We match you with the best car options based on what you care about</p>
-            <p>🚗 You get matched with cars tailored to your lifestyle</p>
-            <button className="bg-accent text-white rounded-lg w-40 p-2 mt-2 ">Find your next car</button>
+    <div className="bg-gray-50 min-h-screen flex flex-col sm:flex-row">
+        <div className="p-7 flex flex-col gap-4 sm:p-7 sm:mt-10">
+            <h1 className="font-bold text-3xl sm:text-5xl">Tell us <span className="text-accent relative after:absolute after:left-0 after:bottom-0 after:h-[2px] after:w-full after:bg-accent after:scale-x-0 hover:after:scale-x-100 after:origin-left after:transition-transform after:duration-300">what matters most</span><br /> - and we'll do the rest</h1>
+            <h2 className="font-medium text-xl sm:text-2xl">We match you with the best car options based on what you care about</h2>
+            <p className="sm:text-lg">🧠 We crunch the information using listings & vehicle data</p>
+            <p className="sm:text-lg">🚗 You get matched with cars tailored to your lifestyle</p>
+            <button className="bg-accent text-white rounded-lg w-40 p-2 mt-2 sm:text-lg sm:w-50" onClick={onFindClick}>Find your next car</button>
         </div>
-        <div>
+        <div className="flex justify-end mt-12 sm:h-120 sm:mt-50">
             <img src="/blueCars.png" />
         </div>
     </div>
@@ -19,3 +19,4 @@ function Landing() {
  }
 
 export default Landing;
+
